582f7131be219a42deb3bd2d2f007ed6058be2d5
remi ibraheem <remicodesmuintu@gmail.com>
2020-08-25 22:53:06 -0400

    added server js and viewjs routes

---
+++ routes/view.js
@@ -0,0 +1,7 @@
+
+// import router
+// import path 
+
+//create route to create a file path for html stats
+
+//export router
---
+++ server.js
@@ -0,0 +1,16 @@
+
+//export express
+// import logger
+//import mongoose 
+
+//create Port
+
+//create express app
+//initialize logger
+//initialize express json and url
+
+//connect Mongoose database
+
+//create rout to require app to route connect to routes folders and files
+
+/// have server listen for request in port
