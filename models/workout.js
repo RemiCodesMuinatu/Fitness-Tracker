d76013f15d461414110c4f0a66ea4e3a01bef33e
remi ibraheem <remicodesmuintu@gmail.com>
2020-08-25 22:26:04 -0400

    added models withindex and workout js files as well as api.js file psuedo codes

---
+++ models/workout.js
@@ -0,0 +1,10 @@
+
+
+// import mongoose package
+
+//Create a mongoose schema
+
+// *********create mongoose schema for workouts *****
+//create schema for day, exercises, name, duration, weight, reps, sets and distances
+
+// export workout module
---
+++ routes/api.js
@@ -0,0 +1,12 @@
+// import router for express
+//import workout module
+
+//create route post information about workout into the data base. stringify the workout for json for the user to see
+
+//create route to put  information into the system and make sure users are putting in the right information
+
+//create a route to get workout information that is stringified
+
+//create a route to get workout range and catch any err in any of the routes. 
+
+//export router module
